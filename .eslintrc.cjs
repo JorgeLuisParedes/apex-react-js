19670eeb53d6cceb60bc9f9e43038ae294ef3b41
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-06-11 01:06:09 +0200

    configuracion eslint-prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,43 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	settings: { react: { version: '18.2' } },
+	extends: [
+		'standard',
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+		'eslint-config-prettier',
+	],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', 'react-refresh'],
+	rules: {
+		'react/jsx-no-target-blank': 'off',
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+		'import/export': 0,
+		'react/no-unescaped-entities': 'off',
+		'react-hooks/exhaustive-deps': 'off',
+		'no-unused-vars': 'off',
+		'import/no-absolute-path': 'off',
+	},
+};
